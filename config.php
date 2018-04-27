6d7d9d4f3c5ba2ac286a57041e05dec3b67fd398
Unknown <irvan.2208@gmail.com>
2018-04-27 12:18:25 +0700

    Update: new custom theme

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'ubuntu');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'tumbuh_baru');
 define('DB_PORT', '3306');
